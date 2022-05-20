def01cea74a8f233c847ce8c373d3e0f3928fd37
guptanikhil801 <guptanikhil801@gmail.com>
2022-05-20 18:49:26 +0530

    [New] Entity Framework configured

---
+++ DatingApp/Api/DataAccess/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// It creates Table of AppUser in Database
+        /// </summary>
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
