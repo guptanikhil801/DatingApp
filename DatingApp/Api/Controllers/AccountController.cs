ccb758437fa790cd0fe6a2fa6d2b92c2a37d27ae
guptanikhil801 <guptanikhil801@gmail.com>
2022-06-03 21:25:58 +0530

    Validation added for Register endpoint

--- DatingApp/Api/Controllers/AccountController.cs
+++ DatingApp/Api/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Api.DataAccess;
 using Api.Entities;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -15,18 +17,30 @@ namespace Api.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register (string UserName, string Password)
+        public async Task<ActionResult<AppUser>> Register(Register register)
         {
-            using var hmac =  new HMACSHA512();
+            if (await UserExists(register.UserName))
+            {
+                return BadRequest();
+            }
+            using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = UserName,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(Password)),//GetBytes converts string into bytes
+                UserName = register.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),//GetBytes converts string into bytes
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
         }
+
+        #region Private Helpers
+        private async Task<bool> UserExists(string UserName)
+        {
+            var user = _context.Users.AnyAsync(x => x.UserName == UserName.ToLower());
+            return await user;
+        }
+        #endregion
     }
 }
