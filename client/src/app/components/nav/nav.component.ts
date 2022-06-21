28cc6ba25d6b6559469b0c142c71759e1ae11c16
Nikhil <guptanikhil801@gmail.com>
2022-06-21 23:05:12 +0530

    [New] After Login navigation done

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
   userName = new FormControl('');
   password = new FormControl('');
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -22,6 +23,7 @@ export class NavComponent implements OnInit {
       Password: this.password.value
     }
     this.accountService.login(data).subscribe(response => {
+      this.router.navigateByUrl('');
       console.log(response);
     }, error => {
       console.log(error);
