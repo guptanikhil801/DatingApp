592fc8cc76a21bacadc526e1428f4d5623c9a55f
Nikhil <guptanikhil801@gmail.com>
2022-06-26 11:45:28 +0530

    [New] Unit Test Project created, also added some tests

---
+++ DatingApp/DatingAppUnitTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Api.Controllers;
+using Api.Entities;
+using Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ApiTests.Controllers
+{
+    public class UserControllerTests
+    {
+        private readonly Mock<IUserRepository> service;
+
+        private AppUser appUser = new AppUser
+        {
+            City = "chaibasa",
+            Country = "india",
+            Create_Ts = DateTime.Now,
+            DateOfBirth = new DateTime(1995, 06, 06),
+            Gender = "Male",
+            Id = 90,
+            Interests = "Music",
+            Introduction = "blah",
+            LastActive = DateTime.Now.AddMinutes(-10d),
+            LookingFor = "Female",
+            NickName = "Nikhil",
+            PasswordHash = new byte[9],
+            PasswordSalt = new byte[8],
+            Photos = null,
+            UserName = "Nikhil"
+        };
+
+        public UserControllerTests()
+        {
+            service = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async void GetUsersAsync_GivenEndpointCalled_ThenAllAppUsersIsReturned()
+        {
+            //arrange
+            List<AppUser> appUsers = new List<AppUser>();
+            appUsers.Add(appUser);
+            IEnumerable<AppUser> allUsers = appUsers;
+            var x = Task.FromResult(allUsers);  // FromResults converts from sync to async( T to Task<T> )
+            service.Setup(x => x.GetUsersAsync()).Returns(x);
+            var controller = new UsersController(service.Object);
+
+            //act
+            var users = await controller.GetUsers();
+            var result = users.Result as OkObjectResult;
+            var actualResult = result.Value as List<AppUser>;
+
+            //Asserts
+            Assert.Equal(200, result.StatusCode);
+            Assert.Single(actualResult);
+            Assert.Equal("chaibasa", actualResult[0].City);
+        }
+    }
+}
---
+++ DatingApp/DatingAppUnitTests/Data/UserRepositoryTests.cs
@@ -0,0 +1,52 @@
+﻿using Api.Data;
+using Api.DataAccess;
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppUnitTests.Data
+{
+    public class UserRepositoryTests
+    {
+        private AppUser appUser = new AppUser
+        {
+            City = "chaibasa",
+            Country = "india",
+            Create_Ts = DateTime.Now,
+            DateOfBirth = new DateTime(1995, 06, 06),
+            Gender = "Male",
+            Id = 90,
+            Interests = "Music",
+            Introduction = "blah",
+            LastActive = DateTime.Now.AddMinutes(-10d),
+            LookingFor = "Female",
+            NickName = "Nikhil",
+            PasswordHash = new byte[9],
+            PasswordSalt = new byte[8],
+            Photos = null,
+            UserName = "Nikhil"
+        };
+
+        public UserRepositoryTests()
+        {
+        }
+
+        [Fact]
+        public async void GetUserByNameAsync_GivenMethodCalled_ThenShouldReturnDataFromDataBase()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "Users") //  using this, instead of going to original DB, it will call this DB
+                .Options;
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = new DataContext(options))
+            {
+                context.Users.Add(appUser);
+                context.SaveChanges();
+                var obj = new UserRepository(context);
+                var user = await obj.GetUserByIdAsync(90);
+                Assert.Equal(90, user.Id);
+                Assert.Equal(DateTime.Now, user.Create_Ts);
+            }
+        }
+    }
+}
